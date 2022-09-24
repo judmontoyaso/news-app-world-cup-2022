ec13d92bbb6146663a15fbe68b1ebc916c5f71ba
Judmontoyaso <juandavidsolorzano73@gmail.com>
2022-09-24 11:45:17 -0500

    mostar noticias de soccer

--- pages/index.js
+++ pages/index.js
@@ -4,34 +4,35 @@ import styles from "../styles/Home.module.css";
 
 export default function Home({ articles }) {
   return (
-    <section className="flex flex-col text-center">
+    <section className="flex flex-col self-center content-center items-center text-center ">
+       <div>
+        <div >Soccer</div>
+
+      </div>
       {articles.length == 0 && <p>Loading ... </p>}
       {articles.length > 0 &&
         articles.map((article, index) => (
-          <div key={index}>
+          <div
+            key={index}
+            className="w-96 mt-10 mb-10 text-center border-gray-900 border-2 "
+          >
             <img alt="image" src={article.urlToImage}></img>
+            <div>
+              <p className="text-3xl font-bold underline">{article.title}</p>
+            </div>
+            <div>
+              <p>{article.description}</p>
+            </div>
           </div>
         ))}
-      <div>
-        <div className="text-3xl font-bold underline">Mania Futbolera</div>
-        <div>Descripción</div>
-      </div>
-      <div className="grid grid-cols-4 gap-4">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
-        <div>7</div>
-      </div>
+     
     </section>
   );
 }
 
 export async function getServerSideProps() {
   const response = await fetch(
-    "https://newsapi.org/v2/everything?q=tesla&from=2022-08-24&sortBy=publishedAt&apiKey=1ff602cc716b4e8aa6c2ac3812e0bb93"
+    "https://newsapi.org/v2/everything?q=soccer-world-cup-&from=2022-08-24&sortBy=publishedAt&apiKey=1ff602cc716b4e8aa6c2ac3812e0bb93"
   );
   const { articles } = await response.json();
   return {
