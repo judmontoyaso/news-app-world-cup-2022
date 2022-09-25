295b3ac8653c4b67f5df8e9d409f8aa5293827cb
Judmontoyaso <juandavidsolorzano73@gmail.com>
2022-09-25 15:42:06 -0500

    Agregar darkmode

---
+++ components/ThemeSwitch.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+
+const ThemeSwitch = () => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme, resolvedTheme } = useTheme();
+
+  // When mounted on client, now we can show the UI
+  useEffect(() => setMounted(true), []);
+
+  return (
+    <button
+      aria-label="Toggle Dark Mode"
+      type="button"
+      className="   
+        focus:outline outline-none"
+      onClick={() =>
+        setTheme(
+          theme === "dark" || resolvedTheme === "dark" ? "light" : "dark"
+        )
+      }
+    >
+      {/* <div className="rounded-full h-6 w-6 bg-gray-200 dark:transform dark:translate-x-7 dark:absolute duration-700 absolute"></div> */}
+
+      <>
+        <FontAwesomeIcon
+          icon={
+            mounted && (theme === "dark" || resolvedTheme === "dark")
+              ? faSun
+              : faMoon
+          }
+          size="1x"
+          className={
+            mounted && (theme === "dark" || resolvedTheme === "dark")
+              ? "text-white text-2xl hover:text-yellow-300 "
+              : "text-black text-2xl hover:text-blue-600"
+          }
+        />
+      </>
+    </button>
+  );
+};
+
+export default ThemeSwitch;
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ import { useState } from "react";
 import {} from "@fortawesome/free-solid-svg-icons";
 import { faNewspaper, faSoccerBall } from "@fortawesome/free-regular-svg-icons";
 import Footer from "../components/Footer";
+import ThemeSwitch from "../components/ThemeSwitch";
+import { useTheme } from "next-themes";
 
 export default function Home({ articles }) {
   const [numberArticles, setNumberArticles] = useState(6);
@@ -15,39 +17,45 @@ export default function Home({ articles }) {
   const articulo = articles.slice(0, numberArticles);
 
   return (
-    <section className="flex flex-col self-center content-center items-center text-center ">
-      <div>
-        <div className="mt-10 mb-10 text-2xl md:text-4xl font-bold text-gray-700">
+    <section className=" self-start content-start items-center text-start dark:bg-gray-900 ">
+      <div className="fixed top-6 left-6">
+        <ThemeSwitch />
+      </div>
+      <div className="flex flex-col self-center content-center items-center text-center  dark:bg-zinc-900 bg-zinc-100">
+        <div className="mt-10 mb-10 text-2xl md:text-4xl font-bold text-gray-700 dark:text-gray-100 ">
           Soccer News <FontAwesomeIcon icon={faSoccerBall} />{" "}
           <FontAwesomeIcon icon={faNewspaper} />
         </div>
-      </div>
-      {articles.length == 0 && <p>Loading ... </p>}
-      {articles.length > 0 &&
-        articulo.map((article, index) => (
-          <div
-            key={index}
-            className="w-72 md:w-96 mt-10 mb-10 text-center border-2  border-gray-400 shadow-slate-200  rounded-lg color bg-slate-200 text-slate-700 "
-          >
-            <img
-              alt={`Image for the article ${article.title}`}
-              src={article.urlToImage}
-            ></img>
-            <div className="p-4 md:p-8">
-              <div>
-                <p className="text-lg md:text-3xl font-bold">{article.title}</p>
-              </div>
-              <div className="text-gray-600 mt-5">
-                <p>{article.description}</p>
-              </div>
-              <div className="mt-6 text-right">
-                <a className="font-bold" href={article.url}>
-                  Read More <FontAwesomeIcon icon={faSoccerBall} />
-                </a>
+
+        {articles.length == 0 && <p>Loading ... </p>}
+        {articles.length > 0 &&
+          articulo.map((article, index) => (
+            <div
+              key={index}
+              className="w-72 md:w-96 mt-10 mb-10 text-center border-2  border-gray-400 shadow-slate-200  rounded-lg color bg-slate-200 text-slate-700 "
+            >
+              <img
+                alt={`Image for the article ${article.title}`}
+                src={article.urlToImage}
+              ></img>
+              <div className="p-4 md:p-8">
+                <div>
+                  <p className="text-lg md:text-3xl font-bold">
+                    {article.title}
+                  </p>
+                </div>
+                <div className="text-gray-600 mt-5">
+                  <p>{article.description}</p>
+                </div>
+                <div className="mt-6 text-right">
+                  <a className="font-bold" href={article.url}>
+                    Read More <FontAwesomeIcon icon={faSoccerBall} />
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+      </div>
       <div
         className="cursor-pointer font-bold text-gray-700 text-xl  hover:text-blue-700"
         onClick={verMas}
@@ -60,7 +68,7 @@ export default function Home({ articles }) {
 }
 
 export async function getServerSideProps() {
-  const day = Date.now;
+  const day = 25;
 
   const response = await fetch(
     `https://newsapi.org/v2/everything?q=soccer-world-cup-&from=2022-08-${day}&sortBy=publishedAt&apiKey=1ff602cc716b4e8aa6c2ac3812e0bb93`
