b3bc9a1687974c3d9b08816e5cbc8188c7d72878
Judmontoyaso <juandavidsolorzano73@gmail.com>
2022-09-25 15:59:03 -0500

    agregar adsense

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,27 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class" enableSystem={false}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <Script
+          id="Adsense-id"
+          data-ad-client="ca-pub-4689214255850199"
+          async="true"
+          strategy="beforeInteractive"
+          onError={(e) => {
+            console.error("Script failed to load", e);
+          }}
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        />
+      </Head>
+      <ThemeProvider attribute="class" enableSystem={false}>
+        <Component {...pageProps} />
+      </ThemeProvider>{" "}
+    </>
   );
 }
 
